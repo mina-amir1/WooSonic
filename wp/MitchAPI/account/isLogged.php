f982c5d916f3ca23124db5cb8a4edb179c5d5b1e
Mina Amir <mina.amir@scandiweb.com>
2023-06-19 15:44:52 +0300

    PWA-25 "Deleting old account endpoint"

--- wp/MitchAPI/account/isLogged.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../wp-load.php';
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: ' . PWA_Base_Link);
-
-$user = wp_get_current_user();
-echo $user->ID;
--- wp/MitchAPI/account/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../../wp-load.php';
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: ' . PWA_Base_Link);
-
-wp_logout();
